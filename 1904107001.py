aaac233b6ed473c2dc2532930824b85817ba5254
omerhubx <66076863+omerhubx@users.noreply.github.com>
2020-07-03 15:16:07 +0300

    Update 1904107001.py

--- 1904107001.py
+++ 1904107001.py
@@ -1,3 +1,5 @@
+#https://github.com/omerhubx/untitled.git
+
 #1.Soru
 
 o= { 1 : "Ocak", 2 : "Şubat" , 3 : "Mart", 4 : "Nisan" , 5 : "Mayıs" , 6: "Haziran" ,
@@ -62,19 +64,67 @@ else:
 
 #3.Soru
 
+o= {1 : "a" , 2 : "b" , 3  :  "c", 4 : "d" , 5 : "e" , 6 : "f" , 7 : "g" , 8 : "h" , 9 : "i" , 10 : "j" , 11 : "k" , 12 : "l",
+    13 :"m" , 14 : "n" , 15 :  "o" , 16 : "p" , 17 : "q" ,18 : "r" , 19 : "s" , 20 : "t" , 21 : "u" , 22 : "v" , 23 :"w",
+    24 : "x" , 25 : "y" , 26 : "z"}
 
 
+m = [[15,13,5]]
+m2 = [[18,8,1]]
+m3 = [[14,15,7]]
+m4 = [[12,21,1]]
 
 
+e = [[0,0,0]]
+e2 = [[0,0,0]]
+e3 = [[0,0,0]]
+e4 = [[0,0,0]]
 
 
+Sifre=[[1,2,-1],[2,5,2],[-1,-2,2]]
+
+for r in range(len(m)):
+     for h in range(len(Sifre[0])):
+
+         for a in range(len(Sifre)):
+             e[r][h] += m[r][a] * Sifre[a][h]
+
+for g in e:
+    print(g)
 
 
+for r in range(len(m2)):
+     for h in range(len(Sifre[0])):
+
+         for a in range(len(Sifre)):
+             e2[r][h] += m2[r][a] * Sifre[a][h]
+
+for g in e2:
+    print(g)
+
+
+for r in range(len(m3)):
+     for h in range(len(Sifre[0])):
+
+         for a in range(len(Sifre)):
+             e3[r][h] += m3[r][a] * Sifre[a][h]
+
+for g in e3:
+    print(g)
+
+for r in range(len(m4)):
+     for h in range(len(Sifre[0])):
+
+         for a in range(len(Sifre)):
+             e4[r][h] += m4[r][a] * Sifre[a][h]
+
+for g in e4:
+    print(g)
 
 #4.Soru
 
 
-print(0, "ile", 10, "arasındaki asal sayılar:")
+print( 0, "ile", 10, "arasındaki asal sayılar:")
 e=[]
 for o in range(0, 10 + 1):
     if o > 1:
